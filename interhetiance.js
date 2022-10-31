4bf2bebbec3cf07f3c14ae30ef0525470f856700
Addddddd <81074236+xadd-145@users.noreply.github.com>
2022-10-31 10:17:49 +0530

    Create interhetiance.js

---
+++ interhetiance.js
@@ -0,0 +1,37 @@
+// Inheritance
+
+// class Rectangle {
+//   constructor(l, b) {
+//     this.l = l
+//     this.b = b
+//   }
+//   calculateArea() {
+//     let a
+//     a = this.l * this.b
+//     return a
+//   }
+//   calculatePerimeter() {
+//     let p
+//     p = 2 * (this.l + this.b)
+//     return p
+//   }
+// }
+
+// class Cuboid extends Rectangle {
+//   constructor(l, b, h) {
+//     super(l, b)
+//     this.h = h
+//   }
+//   calculateVolume() {
+//     let v
+//     v = this.l * this.b * this.h
+//     return v
+//   }
+// }
+
+// let r1 = new Cuboid(5, 6, 7)
+// let area = r1.calculateArea()
+// let perimeter = r1.calculatePerimeter()
+// let volume = r1.calculateVolume()
+
+//console.log(area, perimeter, volume)
